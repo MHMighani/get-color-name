ce4981f5539540b8f97ec3c796ce3ae5ef45e9f6
MHMighani <mhmighani.dev@gmail.com>
2024-12-21 23:20:01 +0330

    chore: better colors data

--- data/index.ts
+++ data/index.ts
@@ -6,327 +6,327 @@ type Color = {
 export const colors: Color[] = [
   {
     name: 'جگری',
-    hex: '۵C۵CCD',
+    hex: 'CD5C5C',
   },
   {
     name: 'بژ تیره',
-    hex: '۸۰۸۰F0',
+    hex: 'F08080',
   },
   {
     name: 'حناییِ روشن',
-    hex: '۷۲۸۰FA',
+    hex: 'FA8072',
   },
   {
     name: 'قهوه‌ایِ حنایی',
-    hex: '۷A۹۶E9',
+    hex: 'E9967A',
   },
   {
     name: 'کرم نارنجی',
-    hex: '۷AA0FF',
+    hex: 'FFA07A',
   },
   {
     name: 'قرمز',
-    hex: '۰۰۰۰FF',
+    hex: 'FF0000',
   },
   {
     name: 'زرشکی',
-    hex: '۳C۱۴DC',
+    hex: 'DC143C',
   },
   {
     name: 'شرابی',
-    hex: '۲۲۲۲B2',
+    hex: 'B22222',
   },
   {
     name: 'عنابی تند',
-    hex: '۰۰۰۰۸B',
+    hex: '8B0000',
   },
   {
     name: 'صورتی',
-    hex: 'CBC0FF',
+    hex: 'FFC0CB',
   },
   {
     name: 'صورتی پررنگ',
-    hex: 'C1B6FF',
+    hex: 'FFB6C1',
   },
   {
     name: 'شرابی روشن',
-    hex: '۹۳۷۰DB',
+    hex: 'DB7093',
   },
   {
     name: 'سرخابی',
-    hex: 'B4۶۹FF',
+    hex: 'FF69B4',
   },
   {
     name: 'شفقی',
-    hex: '۹۳۱۴FF',
+    hex: 'FF1493',
   },
   {
     name: 'ارغوانی',
-    hex: '۸۵۱۵C7',
+    hex: 'C71585',
   },
   {
     name: 'نارنجی کرم',
-    hex: '۷AA0FF',
+    hex: 'FFA07A',
   },
   {
     name: 'نارنجی',
-    hex: '۰۰A5FF',
+    hex: 'FFA500',
   },
   {
     name: 'نارنجی سیر',
-    hex: '۰۰۸CFF',
+    hex: 'FF8C00',
   },
   {
     name: 'نارنجی پررنگ',
-    hex: '۵۰۷FFF',
+    hex: 'FF7F50',
   },
   {
     name: 'قرمز گوجه‌ای',
-    hex: '۴۷۶۳FF',
+    hex: 'FF6347',
   },
   {
     name: 'قرمز-نارنجی',
-    hex: '۰۰۴۵FF',
+    hex: 'FF4500',
   },
   {
     name: 'شیری',
-    hex: 'E0FFFF',
-  },
-  {
-    name: 'شیرشکری',
-    hex: 'CDFAFF',
-  },
-  {
-    name: 'لیمویی روشن',
-    hex: 'D2FAFA',
-  },
-  {
-    name: 'هلویی روشن',
-    hex: 'D5EFFF',
-  },
-  {
-    name: 'هلویی',
-    hex: 'B5E4FF',
-  },
-  {
-    name: 'هلویی پررنگ',
-    hex: 'B9DAFF',
-  },
-  {
-    name: 'نخودی',
-    hex: 'AAE8EE',
-  },
-  {
-    name: 'خاکی',
-    hex: '۸۳E6F0',
-  },
-  {
-    name: 'زرد',
-    hex: '۰۰FFFF',
-  },
-  {
-    name: 'کهربایی باز',
-    hex: '۰۰D7FF',
-  },
-  {
-    name: 'ماشی',
-    hex: '۶BB7BD',
-  },
-  {
-    name: 'مغزپسته‌ای',
-    hex: '۲FFFAD',
-  },
-  {
-    name: 'سبز روشن',
-    hex: '۰۰FF۷F',
-  },
-  {
-    name: 'مغزپسته‌ای پررنگ',
-    hex: '۰۰FC۷C',
-  },
-  {
-    name: 'مغزپسته‌ای',
-    hex: '۰۰FF۰۰',
-  },
-  {
-    name: 'سبز کمرنگ',
-    hex: '۹۸FB۹۸',
-  },
-  {
-    name: 'سبز کدر',
-    hex: '۹۰EE۹۰',
-  },
-  {
-    name: 'یشمی سیر',
-    hex: '۹AFA۰۰',
-  },
-  {
-    name: 'یشمی کمرنگ',
-    hex: '۷FFF۰۰',
-  },
-  {
-    name: 'سبز لجنی',
-    hex: '۳۲CD۹A',
-  },
-  {
-    name: 'سبز چمنی',
-    hex: '۳۲CD۳۲',
-  },
-  {
-    name: 'خزه‌ای',
-    hex: '۷۱B3۳C',
-  },
-  {
-    name: 'خزه‌ای پررنگ',
-    hex: '۵۷۸B۲E',
-  },
-  {
-    name: 'شویدی',
-    hex: '۲۲۸B۲۲',
-  },
-  {
-    name: 'سبز',
-    hex: '۰۰۸۰۰۰',
-  },
-  {
-    name: 'سبز ارتشی',
-    hex: '۲۳۸E۶B',
-  },
-  {
-    name: 'زیتونی',
-    hex: '۰۰۸۰۸۰',
-  },
-  {
-    name: 'زیتونی سیر',
-    hex: '۲F۶B۵۵',
-  },
-  {
-    name: 'سبز آووکادو',
-    hex: '۰۰۶۴۰۰',
-  },
-  {
-    name: 'سبز دریایی',
-    hex: 'AACD۶۶',
-  },
-  {
-    name: 'سبز دریایی تیره',
-    hex: '۸FBC۸F',
-  },
-  {
-    name: 'سبز کبریتی روشن',
-    hex: 'AAB2۲۰',
-  },
-  {
-    name: 'سبز کبریتی تیره',
-    hex: '۸B۸B۰۰',
-  },
-  {
-    name: 'سبز دودی',
-    hex: '۸۰۸۰۰۰',
-  },
-  {
-    name: 'فیروزه‌ای',
-    hex: 'FFFF۰۰',
-  },
-  {
-    name: 'آبی آسمانی',
     hex: 'FFFFE0',
   },
   {
+    name: 'شیرشکری',
+    hex: 'FFFACD',
+  },
+  {
+    name: 'لیمویی روشن',
+    hex: 'FAFAD2',
+  },
+  {
+    name: 'هلویی روشن',
+    hex: 'FFEFD5',
+  },
+  {
+    name: 'هلویی',
+    hex: 'FFE4B5',
+  },
+  {
+    name: 'هلویی پررنگ',
+    hex: 'FFDAB9',
+  },
+  {
+    name: 'نخودی',
+    hex: 'EEE8AA',
+  },
+  {
+    name: 'خاکی',
+    hex: 'F0E683',
+  },
+  {
+    name: 'زرد',
+    hex: 'FFFF00',
+  },
+  {
+    name: 'کهربایی باز',
+    hex: 'FFD700',
+  },
+  {
+    name: 'ماشی',
+    hex: 'BDB76B',
+  },
+  {
+    name: 'مغزپسته‌ای',
+    hex: 'ADFF2F',
+  },
+  {
+    name: 'سبز روشن',
+    hex: '7FFF00',
+  },
+  {
+    name: 'مغزپسته‌ای پررنگ',
+    hex: '7CFC00',
+  },
+  {
+    name: 'مغزپسته‌ای',
+    hex: '00FF00',
+  },
+  {
+    name: 'سبز کمرنگ',
+    hex: '98FB98',
+  },
+  {
+    name: 'سبز کدر',
+    hex: '90EE90',
+  },
+  {
+    name: 'یشمی سیر',
+    hex: '00FA9A',
+  },
+  {
+    name: 'یشمی کمرنگ',
+    hex: '00FF7F',
+  },
+  {
+    name: 'سبز لجنی',
+    hex: '9ACD32',
+  },
+  {
+    name: 'سبز چمنی',
+    hex: '32CD32',
+  },
+  {
+    name: 'خزه‌ای',
+    hex: '3CB371',
+  },
+  {
+    name: 'خزه‌ای پررنگ',
+    hex: '2E8B57',
+  },
+  {
+    name: 'شویدی',
+    hex: '228B22',
+  },
+  {
+    name: 'سبز',
+    hex: '008000',
+  },
+  {
+    name: 'سبز ارتشی',
+    hex: '6B8E23',
+  },
+  {
+    name: 'زیتونی',
+    hex: '808000',
+  },
+  {
+    name: 'زیتونی سیر',
+    hex: '556B2F',
+  },
+  {
+    name: 'سبز آووکادو',
+    hex: '006400',
+  },
+  {
+    name: 'سبز دریایی',
+    hex: '66CDAA',
+  },
+  {
+    name: 'سبز دریایی تیره',
+    hex: '8FBC8F',
+  },
+  {
+    name: 'سبز کبریتی روشن',
+    hex: '20B2AA',
+  },
+  {
+    name: 'سبز کبریتی تیره',
+    hex: '008B8B',
+  },
+  {
+    name: 'سبز دودی',
+    hex: '008080',
+  },
+  {
+    name: 'فیروزه‌ای',
+    hex: '00FFFF',
+  },
+  {
+    name: 'آبی آسمانی',
+    hex: 'E0FFFF',
+  },
+  {
     name: 'فیروزه‌ای کدر',
-    hex: 'EEEEAF',
+    hex: 'AFEEEE',
   },
   {
     name: 'آبی دریایی',
-    hex: 'FFFF۰۰',
+    hex: '00FFFF',
   },
   {
     name: 'یشمی',
-    hex: 'D4FF۷F',
+    hex: '7FFFD4',
   },
   {
     name: 'سبز دریایی روشن',
-    hex: 'D0E0۴۰',
+    hex: '40E0D0',
   },
   {
     name: 'فیروزه‌ای تیره',
-    hex: 'CCD1۴۸',
+    hex: '48D1CC',
   },
   {
     name: 'فیروزه‌ای سیر',
-    hex: 'D1CE۰۰',
+    hex: '00CED1',
   },
   {
     name: 'آبی کبریتی روشن',
-    hex: 'E6E0B0',
+    hex: 'B0E0E6',
   },
   {
     name: 'بنفش مایل به آبی',
-    hex: 'DEC4B0',
+    hex: 'B0C4DE',
   },
   {
     name: 'آبی کبریتی',
-    hex: 'E6D8AD',
+    hex: 'ADD8E6',
   },
   {
     name: 'آبی آسمانی سیر',
-    hex: 'EBCE۸۷',
+    hex: '87CEEB',
   },
   {
     name: 'آبی روشن',
-    hex: 'FACE۸۷',
+    hex: '87CEFA',
   },
   {
     name: 'آبی کمرنگ',
-    hex: 'FFBF۰۰',
+    hex: '00BFFF',
   },
   {
     name: 'آبی کدر',
-    hex: 'ED۹۵۶۴',
+    hex: '6495ED',
   },
   {
     name: 'نیلی متالیک',
-    hex: 'B4۸۲۴۶',
+    hex: '4682B4',
   },
   {
     name: 'آبی لجنی',
-    hex: 'A0۹E۵F',
+    hex: '5F9EA0',
   },
   {
     name: 'آبی متالیک روشن',
-    hex: 'EE۶۸۷B',
+    hex: '7B68EE',
   },
   {
     name: 'نیلی',
-    hex: 'FF۹۰۱E',
+    hex: '1E90FF',
   },
   {
     name: 'فیروزه‌ای فسفری',
-    hex: 'E1۶۹۴۱',
+    hex: '4169E1',
   },
   {
     name: 'آبی',
-    hex: 'FF۰۰۰۰',
+    hex: '0000FF',
   },
   {
     name: 'آبی سیر',
-    hex: 'CD۰۰۰۰',
+    hex: '0000CD',
   },
   {
     name: 'سرمه‌ای',
-    hex: '۸B۰۰۰۰',
+    hex: '00008B',
   },
   {
     name: 'لاجوردی',
-    hex: '۸۰۰۰۰۰',
+    hex: '000080',
   },
   {
     name: 'آبی نفتی',
-    hex: '۷۰۱۹۱۹',
+    hex: '191970',
   },
   {
     name: 'نیلی کمرنگ',
-    hex: 'FAE6E6',
+    hex: 'E6E6FA',
   },
   {
     name: 'بادمجانی روشن',
@@ -338,127 +338,127 @@ export const colors: Color[] = [
   },
   {
     name: 'بنفش روشن',
-    hex: 'EE۸۲EE',
+    hex: 'EE82EE',
   },
   {
     name: 'سرخابی',
-    hex: 'FF۰۰FF',
+    hex: 'FF00FF',
   },
   {
     name: 'سرخابی روشن',
-    hex: 'FF۰۰FF',
+    hex: 'FF00FF',
   },
   {
     name: 'ارکیده',
-    hex: 'D6۷۰DA',
+    hex: 'DA70D6',
   },
   {
     name: 'ارکیده سیر',
-    hex: 'D3۵۵BA',
+    hex: 'BA55D3',
   },
   {
     name: 'آبی بنفش',
-    hex: 'DB۷۰۹۳',
+    hex: '9370DB',
   },
   {
     name: 'آبی فولادی',
-    hex: 'CD۵A۶A',
+    hex: '6A5ACD',
   },
   {
     name: 'آبی-بنفش سیر',
-    hex: 'E2۲B۸A',
+    hex: '8A2BE2',
   },
   {
     name: 'بنفش باز',
-    hex: 'D3۰۰۹۴',
+    hex: '9400D3',
   },
   {
     name: 'ارکیده بنفش',
-    hex: 'CC۳۲۹۹',
+    hex: '9932CC',
   },
   {
     name: 'مخملی',
-    hex: '۸B۰۰۸B',
+    hex: '8B008B',
   },
   {
     name: 'بنفش',
-    hex: '۸۰۰۰۸۰',
+    hex: '800080',
   },
   {
     name: 'آبی دودی',
-    hex: '۸B۳D۴۸',
+    hex: '483D8B',
   },
   {
     name: 'نیلی سیر',
-    hex: '۸۲۰۰۴B',
+    hex: '4B0082',
   },
   {
     name: 'کاهی',
-    hex: 'DCF8FF',
+    hex: 'FFF8DC',
   },
   {
     name: 'کاهگلی',
-    hex: 'CDEBFF',
+    hex: 'FFEBCD',
   },
   {
     name: 'کرم',
-    hex: 'C4E4FF',
+    hex: 'FFE4C4',
   },
   {
     name: 'کرم سیر',
-    hex: 'ADDEFF',
+    hex: 'FFDEAD',
   },
   {
     name: 'گندمی',
-    hex: 'BCDEF5',
+    hex: 'F5DEBC',
   },
   {
     name: 'خاکی',
-    hex: '۸۷B8DE',
+    hex: 'DEB887',
   },
   {
     name: 'برنزه کدر',
-    hex: '۸CB4D2',
+    hex: 'D2B48C',
   },
   {
     name: 'بادمجانی',
-    hex: '۸F۸FBC',
+    hex: 'BC8F8F',
   },
   {
     name: 'هلویی سیر',
-    hex: '۶۰A4F4',
+    hex: 'F4A460',
   },
   {
     name: 'خردلی',
-    hex: '۲۰A5DA',
+    hex: 'DAA520',
   },
   {
     name: 'ماشی سیر',
-    hex: '0B۸۶B8',
+    hex: 'B8860B',
   },
   {
     name: 'بادامی سیر',
-    hex: '۳F۸۵CD',
+    hex: 'CD853F',
   },
   {
     name: 'عسلی پررنگ',
-    hex: '۱E۶۹D2',
+    hex: 'D2691E',
   },
   {
     name: 'کاکائویی',
-    hex: '۱۳۴۵۸B',
+    hex: '8B4513',
   },
   {
     name: 'قهوه‌ای متوسط',
-    hex: '۲D۵۲A0',
+    hex: 'A0522D',
   },
   {
     name: 'قهوه‌ای',
-    hex: '۲A۲AA5',
+    hex: 'A52A2A',
   },
   {
     name: 'آلبالویی',
-    hex: '۰۰۰۰۸۰',
+    hex: '800000',
   },
   {
     name: 'سفید',
@@ -466,7 +466,7 @@ export const colors: Color[] = [
   },
   {
     name: 'صورتی محو',
-    hex: 'FAFAFF',
+    hex: 'FFFAFA',
   },
   {
     name: 'یشمی محو',
@@ -474,19 +474,19 @@ export const colors: Color[] = [
   },
   {
     name: 'سفید نعنائی',
-    hex: 'FAFFF5',
+    hex: 'F5FFFA',
   },
   {
     name: 'آبی محو',
-    hex: 'FFFFF0',
+    hex: 'F0FFFF',
   },
   {
     name: 'نیلی محو',
-    hex: 'FFF8F0',
+    hex: 'F0F8FF',
   },
   {
     name: 'سفید بنفشه',
-    hex: 'FFF8F8',
+    hex: 'F8F8FF',
   },
   {
     name: 'خاکستری محو',
@@ -494,39 +494,39 @@ export const colors: Color[] = [
   },
   {
     name: 'بژ باز',
-    hex: 'EEF5FF',
+    hex: 'FFF5EE',
   },
   {
     name: 'هِلی',
-    hex: 'DCF5F5',
+    hex: 'F5F5DC',
   },
   {
     name: 'بژ روشن',
-    hex: 'E6F5FD',
+    hex: 'FDF5E6',
   },
   {
     name: 'پوست پیازی',
-    hex: 'F0FAFF',
+    hex: 'FFFAF0',
   },
   {
     name: 'استخوانی',
-    hex: 'F0FFFF',
+    hex: 'FFFFF0',
   },
   {
     name: 'بژ تیره',
-    hex: 'D7EBFA',
+    hex: 'FAEBD7',
   },
   {
     name: 'کتانی',
-    hex: 'E6F0FA',
+    hex: 'FAF0E6',
   },
   {
     name: 'صورتی مات',
-    hex: 'F5F0FF',
+    hex: 'FFF0F5',
   },
   {
     name: 'بژ',
-    hex: 'E1E4FF',
+    hex: 'FFE4E1',
   },
   {
     name: 'خاکستری مات',
@@ -546,26 +546,26 @@ export const colors: Color[] = [
   },
   {
     name: 'خاکستری',
-    hex: '۸۰۸۰۸۰',
+    hex: '808080',
   },
   {
     name: 'دودی',
-    hex: '۶۹۶۹۶۹',
+    hex: '696969',
   },
   {
     name: 'سربی',
-    hex: '۹۹۸۸۷۷',
+    hex: '778899',
   },
   {
     name: 'سربی تیره',
-    hex: '۹۰۸۰۷۰',
+    hex: '708090',
   },
   {
     name: 'لجنی تیره',
-    hex: '۴F۴F۲F',
+    hex: '2F4F4F',
   },
   {
     name: 'سیاه',
-    hex: '۰۰۰۰۰۰',
+    hex: '000000',
   },
 ];
