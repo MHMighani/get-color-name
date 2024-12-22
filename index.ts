4e6b0d9275f837c1f1e1a5730b029957e2182196
MHMighani <mhmighani.dev@gmail.com>
2024-12-22 22:52:12 +0330

    chore: init find color name function

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import { colors } from './data';
+import chroma from 'chroma-js';
+
+export const getColorName = (color: string) => {
+  const deltas = colors.map((c) => chroma.deltaE(color, c.hex));
+  const min = Math.min(...deltas);
+
+  const index = deltas.indexOf(min);
+
+  return colors[index];
+};
