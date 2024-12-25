76c02b45509e17a48dd163224f89ceb8400f5a1a
MHMighani <mhmighani.dev@gmail.com>
2024-12-25 20:20:53 +0330

    chore: add tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,12 +2,20 @@ import { getColorName } from '..';
 
 describe('getColorName function persian colors', () => {
   it('should return قرمز for #FF0000', () => {
-    expect(getColorName('#FF0000')).toBe('قرمز');
+    expect(getColorName('#FF0000', 'fa')).toBe('قرمز');
+  });
+
+  it('should return بنفش for #841474', () => {
+    expect(getColorName('#841474', 'fa')).toBe('بنفش');
   });
 });
 
 describe('getColorName function english colors', () => {
   it('should return red for #FF0000', () => {
     expect(getColorName('#FF0000', 'en')).toBe('Red');
+  });
+
+  it('should return purple for #841474', () => {
+    expect(getColorName('#841474', 'en')).toBe('Purple');
   });
 });
