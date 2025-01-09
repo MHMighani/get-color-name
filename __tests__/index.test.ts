2fea470d64101354dbcef4cc0c25bd0f228c26fb
MHMighani <mhmighani.dev@gmail.com>
2025-01-09 20:40:14 +0330

    chore: add tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { getColorName } from '..';
+import { getColorCode, getColorName } from '../src/index.ts';
 
 describe('getColorName function persian colors', () => {
   it('should return قرمز for #FF0000', () => {
@@ -17,5 +17,25 @@ describe('getColorName function english colors', () => {
 
   it('should return purple for #841474', () => {
     expect(getColorName('#841474', 'en')).toBe('Purple');
+  });
+});
+
+describe('color codes for literal english names', () => {
+  it('should return #FF0000 for red color', () => {
+    expect(getColorCode('red')).toBe('#ff0000');
+  });
+
+  it('should return undefined for unknown-color', () => {
+    expect(getColorCode('unknown')).toBe(undefined);
+  });
+});
+
+describe('color codes for literal persian names', () => {
+  it('should return #FF0000 for قرمز', () => {
+    expect(getColorCode('قرمز', 'fa')).toBe('#ff0000');
+  });
+
+  it('should return undefined for رنگی که وجود ندارد', () => {
+    expect(getColorCode('رنگی که وجود ندارد', 'fa')).toBe(undefined);
   });
 });
